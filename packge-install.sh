318b24844834cff28508979b3ac460e08581acb1
Jeetaditya Chatterjee <jeeta@chatterlap.localdomain>
2020-05-02 22:44:40 +0100

    working on my package install script

--- packge-install.sh
+++ packge-install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin env sh
 
-sudo apt-get install zsh neovim emacs deja-dup python3-pip snapd gnome-tweaks herbstluftwm 
+basics=(zsh alacritty neovim emacs)
+code=(golang python3-pip rustc)
+GNOME=(gnome-pomadoro gnome-backgrounds gnome-paint)
+
+sudo apt-get install 
 wget -qO- https://git.io/papirus-icon-theme-install | sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
