687368f2e4978a27ad8c8223e8e0d964bd7cffdd
Jeetaditya Chatterjee <jeeta@chatterlap.localdomain>
2020-05-03 02:09:26 +0100

    adding more programs

--- packge-install.sh
+++ packge-install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin env sh
 packages=(
     basics=(zsh alacritty neovim emacs)
-    lang=(golang python3-pip rustc)
+    lang=(golang python3-pip rustc texlive-full)
     term=(xdotool tree)
     GNOME=(gnome-pomadoro gnome-backgrounds gnome-paint)
     appformats=(flatpak snapd)
-    games=(ninvaders moonbuggy)
+    games=(ninvaders moonbuggy neofetch)
 )
 sudo apt-get install $packages
 wget -qO- https://git.io/papirus-icon-theme-install | sh
