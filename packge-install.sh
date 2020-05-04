27ef82066942abb01271a0b137834183748fbdcc
Jeetaditya Chatterjee <jeeta@chatterlap.localdomain>
2020-05-04 02:07:40 +0100

    adding packages to package install

--- packge-install.sh
+++ packge-install.sh
@@ -2,12 +2,13 @@
 packages=(
     basics=(zsh alacritty neovim emacs)
     lang=(golang python3-pip rustc texlive-full)
-    term=(xdotool tree)
-    GNOME=(gnome-pomadoro gnome-backgrounds gnome-paint)
+    term=(xdotool tree neofetch)
+    GNOME=(gnome-shell-pomodoro gnome-backgrounds gnome-paint)
     appformats=(flatpak snapd)
-    games=(ninvaders moonbuggy neofetch)
+    games=(ninvaders moonbuggy)
 )
-sudo apt-get install $packages
+
+sudo apt-get install $packages 
 wget -qO- https://git.io/papirus-icon-theme-install | sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
