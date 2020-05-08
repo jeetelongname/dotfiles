fec55ae84ae0c6af075d211aa46e0bb617d1810b
Jeetaditya Chatterjee <jeeta@chatterlap.localdomain>
2020-05-09 00:43:18 +0100

    fidling with the package install (still does not work)

--- packge-install.sh
+++ packge-install.sh
@@ -2,13 +2,12 @@
 
 packages=(zsh alacritty neovim emacs
           golang python3-pip rustc texlive-full
-          xdotool tree neofetch git
+          xdotool tree neofetch git aptitude
           gnome-shell-pomodoro gnome-backgrounds gnome-paint dconf-editor gnome-tweaks
           flatpak snapd
           notrogen 
           ninvaders moonbuggy figlet lolcat cowsay
           libavcodec-extra)
-echo ${packages[*]}
-# sudo apt-get install < $packages 
+sudo apt-get install < $("${packages[*]}"| xargs)
 # wget -qO- https://git.io/papirus-icon-theme-install | sh
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
