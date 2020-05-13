f332998f2d8a1d986ab9ade9b1b944c114fee6da
Jeetaditya Chatterjee <jeeta@chatterlap.localdomain>
2020-05-14 00:49:54 +0100

    made some changes to the tabs in my doom config and used those changes to get my package install working

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -7,24 +7,12 @@
 ;; clients, file templates and snippets.
 (setq user-full-name "Jeetaditya Chatterjee"
       user-mail-address "jeetelongname@gmail.com"
-      ;; pretty self explanitory
-      doom-theme 'doom-horizon
+      doom-theme 'doom-horizon  ; pretty self explanitory
       treemacs-width 30
-      ;; my line numbers count away from the line im on easier to jump to lines
-      display-line-numbers-type 'relative
-      ;; i like my autocomplete like my tea fast and always
-      company-idle-delay 0)
+      display-line-numbers-type 'relative ; i like my autocomplete like my tea fast and always
+      company-idle-delay 1)
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
 (setq doom-font
       (font-spec
        :family "Inconsolata Nerd Font"
@@ -37,10 +25,6 @@
       (font-spec
        :family "Inconsolata Nerd Font"
        :size 15))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -56,8 +40,17 @@
       doom-modeline-env-version t
       doom-modeline-env-enable-python 't
       doom-modeline-env-enable-go 't)
+;; changes to the tab bar
+
+(setq centaur-tabs-style "slant"
+      centaur-tabs-height 25
+      centaur-tabs-show-navigation-buttons t
+      centaur-tabs-set-bar 'under
+      centaur-tabs-close-button "×"
+      centaur-tabs-modified-marker "Ø"
+      )
+
 ;; Here are some additional functions/macros that could help you configure Doom:
-;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package' for configuring packages
 ;; - `after!' for running code after a package has loaded
--- packge-install.sh
+++ packge-install.sh
@@ -1,14 +1,15 @@
-#!/usr/bin env bash
+#!/usr/bin/bash
 
-packages=(zsh alacritty neovim emacs
-          golang python3-pip rustc texlive-full markdown
-          xdotool tree neofetch git aptitude 
-          gnome-shell-pomodoro gnome-backgrounds gnome-paint dconf-editor gnome-tweaks
-          flatpak snapd
-          notrogen 
-          ninvaders moonbuggy figlet lolcat cowsay
-          libavcodec-extra)
-
-sudo apt-get install < $("${packages[*]}"| xargs)
+echo "yeet"
+packages=(zsh alacritty neovim emacs nitrogen build-essential ## the basics (goes on any system no natter what)
+          golang python3-pip rustc texlive-full markdown ## the languages and the backends that i use
+          xdotool tree neofetch git aptitude pandoc ## some nice commandline apps tht i use
+          gnome-shell-pomodoro gnome-backgrounds gnome-paint dconf-editor gnome-tweaks ## GNOME apps (because i use Gnome)
+          flatpak snapd ## all the apps
+          ninvaders moon-buggy figlet lolcat cowsay ##games and all that fun stuff
+          libavcodec-extra hplip ## libs codecs and extras
+          ssh sshfs ## all the ssh
+         )
+echo ${packages[*]} | xargs sudo apt install -y
 # wget -qO- https://git.io/papirus-icon-theme-install | sh
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
