3f08a6a63417a4f9810cfc1857b5fa1ea9406fd1
Jeetaditya Chatterjee <jeeta@chatterlap.localdomain>
2020-05-20 18:39:54 +0100

    removing some stuff from package install

--- packge-install.sh
+++ packge-install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-packages=(zsh alacritty neovim emacs nitrogen build-essential ## the basics (goes on any system no natter what)
+packages=(zsh alacritty tmux neovim emacs nitrogen build-essential ## the basics (goes on any system no matter what)
           golang python3-pip rustc texlive-full markdown ## the languages and the backends that i use
           xdotool tree neofetch git aptitude pandoc ## some nice commandline apps tht i use
-          gnome-shell-pomodoro gnome-backgrounds gnome-paint dconf-editor gnome-tweaks ## GNOME apps (because i use Gnome)
+          gnome-shell-pomodoro gnome-backgrounds dconf-editor gnome-tweaks ## GNOME apps (because i use Gnome)
           flatpak snapd ## all the apps
           ninvaders moon-buggy figlet lolcat cowsay ##games and all that fun stuff
           libavcodec-extra hplip ## libs codecs and extras
