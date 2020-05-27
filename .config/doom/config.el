addcbc4ba3aa839152475fcb8bfbb877167b20e6
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2020-05-27 16:34:23 +0100

    adding an xresources and updating some configs

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -5,7 +5,7 @@
 
 (setq user-full-name "Jeetaditya Chatterjee"
       user-mail-address "jeetelongname@gmail.com"
-      doom-theme 'doom-horizon  ; pretty self explanitory
+      doom-theme 'doom-snazzy; pretty self explanitory
       treemacs-width 30
       display-line-numbers-type 'relative
       company-idle-delay 0.5) ; i like my autocomplete like my tea fast and always
@@ -41,30 +41,31 @@
       doom-modeline-icon(display-graphic-p)
       doom-modeline-major-mode-color-icon t
       doom-modeline-buffer-modification-icon t
-      doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode)
-      )
+      doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
 ;; changes to the tab bar
 
 (setq centaur-tabs-style "zigzag"
       centaur-tabs-height 5
       centaur-tabs-show-navigation-buttons t
-      centaur-tabs-set-bar 'under
+      centaur-tabs-set-bar 'left
       x-underline-at-descent-line t
       centaur-tabs-close-button "×"
-      centaur-tabs-modified-marker "Ø"
-      )
+      centaur-tabs-modified-marker "Ø")
 
 (setq fancy-splash-image "~/.config/doom/icons/emacs-icon.png")
-(custom-theme-set-faces!
-  '(doom-dashboard-footer :inherit font-lock-constant-face)
-  '(doom-dashboard-footer-icon :inherit all-the-icons-red)
-  '(doom-dashboard-loaded :inherit font-lock-warning-face)
-  '(doom-dashboard-menu-desc :inherit font-lock-string-face)
-  '(doom-dashboard-menu-title :inherit font-lock-function-name-face))
+
+(setq wttrin-default-accept-language
+      '("Accept-Language" . "en-gb")
+      wttrin-default-cities
+      '("London"))
+
+(setq +mu4e-backend 'offlineimap)
+
+
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
-;; - `load!' for loading external *.el files relative to this one
+;; - `load!' for loading external *.el files to relative this one
 ;; - `use-package' for configuring packages
 ;; - `after!' for running code after a package has loaded
 ;; - `add-load-path!' for adding directories to the `load-path', relative to
--- .config/doom/init.el
+++ .config/doom/init.el
@@ -166,7 +166,7 @@
        web               ; the tubes
 
        :email
-       ;;(mu4e +gmail)
+       (mu4e +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
