15fd1fd027e5c9a72596e77c9f2b2ae873640cdf
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2020-09-05 20:16:17 +0100

    Small changes

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -14,6 +14,9 @@
 
 (setq fancy-splash-image (concat doom-private-dir "icons/emacs-icon.png"))
 
+;; (setq easy-hugo-basedir "~/code/git-repos/mine/jeetelongname.github.io/blog-hugo/")
+(setq easy-hugo-root "~/code/git-repos/mine/jeetelongname.github.io/blog-hugo/")
+
 (use-package! discord-emacs ;; for face value discord intergration
   :config
   ;; (discord-emacs-run "747913611426529440") ;;mine
@@ -38,9 +41,8 @@
     '(keycast-key :inherit custom-modified
                   :height 1.1
                   :weight bold)))
-
-;; (setq easy-hugo-basedir "~/code/git-repos/mine/jeetelongname.github.io/blog-hugo/")
-(setq easy-hugo-root "~/code/git-repos/mine/jeetelongname.github.io/blog-hugo/")
+;; (map! :leader
+;;       :desc "t k" #'keycast-mode)
 
 (after! company
   (setq company-idle-delay 0.3 ; I like my autocomplete like my tea fast and always
@@ -164,83 +166,123 @@
 (after! mu4e
   (setq +mu4e-backend 'offlineimap
         mail-user-agent 'mu4e-user-agent
-        mu4e-view-use-gnus t)
+        mu4e-view-use-gnus t))
 
+(after! mu4e
+  (defun my-string-width (str)
+    "Return the width in pixels of a string in the current
+window's default font. If the font is mono-spaced, this
+will also be the width of all other printable characters."
+    (let ((window (selected-window))
+          (remapping face-remapping-alist))
+      (with-temp-buffer
+        (make-local-variable 'face-remapping-alist)
+        (setq face-remapping-alist remapping)
+        (set-window-buffer window (current-buffer))
+        (insert str)
+        (car (window-text-pixel-size)))))
+
+
+  (cl-defun mu4e~normalised-icon (name &key set colour height v-adjust)
+    "Convert :icon declaration to icon"
+    (let* ((icon-set (intern (concat "all-the-icons-" (or set "faicon"))))
+           (v-adjust (or v-adjust 0.02))
+           (height (or height 0.8))
+           (icon (if colour
+                     (apply icon-set `(,name :face ,(intern (concat "all-the-icons-" colour)) :height ,height :v-adjust ,v-adjust))
+                   (apply icon-set `(,name  :height ,height :v-adjust ,v-adjust))))
+           (icon-width (my-string-width icon))
+           (space-width (my-string-width " "))
+           (space-factor (- 2 (/ (float icon-width) space-width))))
+      (concat (propertize " " 'display `(space . (:width ,space-factor))) icon)
+      ))
+
+  (defun mu4e~initialise-icons ()
   (setq mu4e-use-fancy-chars t
-        mu4e-headers-draft-mark     '("D" . " ")
-        mu4e-headers-flagged-mark   '("F" . " ")
-        mu4e-headers-new-mark       '("N" . " ")
-        mu4e-headers-passed-mark    '("P" . " ")
-        mu4e-headers-replied-mark   '("R" . " ")
-        mu4e-headers-seen-mark      '("S" . " ")
-        mu4e-headers-trashed-mark   '("T" . " ")
-        mu4e-headers-attach-mark    '("a" . " ")
-        mu4e-headers-encrypted-mark '("x" . "")
-        mu4e-headers-signed-mark    '("s" . " ")
-        mu4e-headers-unread-mark    '("u" . " "))
-  (setq mu4e-headers-fields '((:human-date . 12)
-                              (:flags      . 4)
-                              (:from       . 25)
-                              (:subject))
+        mu4e-headers-draft-mark      (cons "D" (mu4e~normalised-icon "pencil"))
+        mu4e-headers-flagged-mark    (cons "F" (mu4e~normalised-icon "flag"))
+        mu4e-headers-new-mark        (cons "N" (mu4e~normalised-icon "sync" :set "material" :height 0.8 :v-adjust -0.10))
+        mu4e-headers-passed-mark     (cons "P" (mu4e~normalised-icon "arrow-right"))
+        mu4e-headers-replied-mark    (cons "R" (mu4e~normalised-icon "arrow-right"))
+        mu4e-headers-seen-mark       (cons "S" (mu4e~normalised-icon "eye" :height 0.6 :v-adjust 0.07 :colour "dsilver"))
+        mu4e-headers-trashed-mark    (cons "T" (mu4e~normalised-icon "trash"))
+        mu4e-headers-attach-mark     (cons "a" (mu4e~normalised-icon "file-text-o" :colour "silver"))
+        mu4e-headers-encrypted-mark  (cons "x" (mu4e~normalised-icon "lock"))
+        mu4e-headers-signed-mark     (cons "s" (mu4e~normalised-icon "certificate" :height 0.7 :colour "dpurple"))
+        mu4e-headers-unread-mark     (cons "u" (mu4e~normalised-icon "eye-slash" :v-adjust 0.05))))
+
+  (if (display-graphic-p)
+      (mu4e~initialise-icons)
+    ;; When it's the server, wait till the first graphical frame
+    (add-hook! 'server-after-make-frame-hook
+      (defun mu4e~initialise-icons-hook ()
+        (when (display-graphic-p)
+          (mu4e~initialise-icons)
+          (remove-hook #'mu4e~initialise-icons-hook))))))
+
+(after! mu4e
+
+  (defun mu4e-header-colourise (str)
+    (let* ((str-sum (apply #'+ (mapcar (lambda (c) (% c 3)) str)))
+           (colour (nth (% str-sum (length mu4e-header-colourised-faces))
+                        mu4e-header-colourised-faces)))
+      (put-text-property 0 (length str) 'face colour str)
+      str))
+
+  (defvar mu4e-header-colourised-faces
+    '(all-the-icons-lblue
+      all-the-icons-purple
+      all-the-icons-blue-alt
+      all-the-icons-green
+      all-the-icons-maroon
+      all-the-icons-yellow
+      all-the-icons-orange))
+
+  (setq mu4e-headers-fields
+        '((:account . 8)
+          (:human-date . 8)
+          (:flags . 6)
+          (:from . 25)
+          (:folder . 10)
+          (:recipnum . 2)
+          (:subject))
         mu4e-headers-date-format "%d/%m/%y"
-        mu4e-headers-time-format "%T"))
+        mu4e-headers-time-format "%T")
 
-;; (after! mu4e
-
-;;   (defun mu4e-header-colourise (str)
-;;     (let* ((str-sum (apply #'+ (mapcar (lambda (c) (% c 3)) str)))
-;;            (colour (nth (% str-sum (length mu4e-header-colourised-faces))
-;;                         mu4e-header-colourised-faces)))
-;;       (put-text-property 0 (length str) 'face colour str)
-;;       str))
-
-;;   (defvar mu4e-header-colourised-faces
-;;     '(all-the-icons-lblue
-;;       all-the-icons-purple
-;;       all-the-icons-blue-alt
-;;       all-the-icons-green
-;;       all-the-icons-maroon
-;;       all-the-icons-yellow
-;;       all-the-icons-orange))
-
-;;   (setq mu4e-headers-fields
-;;         '((:account . 12)
-;;           (:human-date . 8)
-;;           (:flags . 6)
-;;           (:from . 25)
-;;           (:folder . 10)
-;;           (:recipnum . 2)
-;;           (:subject))
-;;         mu4e-headers-date-format "%d/%m/%y"
-;;         mu4e-headers-time-format "%T")
-
-;;   (plist-put (cdr (assoc :flags mu4e-header-info)) :shortname " Flags") ; default=Flgs
-;;   (setq mu4e-header-info-custom
-;;         '((:account .
-;;            (:name "Account" :shortname "Account" :help "Which account this email belongs to" :function
-;;             (lambda (msg)
-;;               (let ((maildir
-;;                      (mu4e-message-field msg :maildir)))
-;;                 (mu4e-header-colourise (replace-regexp-in-string "^gmail" (propertize "g" 'face 'bold-italic)
-;;                                                                  (format "%s"
-;;                                                                          (substring maildir 1
-;;                                                                                     (string-match-p "/" maildir 1)))))))))
-;;           (:folder .
-;;            (:name "Folder" :shortname "Folder" :help "Lowest level folder" :function
-;;             (lambda (msg)
-;;               (let ((maildir
-;;                      (mu4e-message-field msg :maildir)))
-;;                 (mu4e-header-colourise (replace-regexp-in-string "\\`.*/" "" maildir))))))
-;;           (:recipnum .
-;;            (:name "Number of recipients"
-;;             :shortname " ⭷"
-;;             :help "Number of recipients for this message"
-;;             :function
-;;             (lambda (msg)
-;;               (propertize (format "%2d"
-;;                                   (+ (length (mu4e-message-field msg :to))
-;;                                      (length (mu4e-message-field msg :cc))))
-;;                           'face 'mu4e-footer-face)))))))
+  (plist-put (cdr (assoc :flags mu4e-header-info)) :shortname " Flags") ; default=Flgs
+  (setq mu4e-header-info-custom
+        '((:account .
+           (:name "Account" :shortname "Account" :help "Which account this email belongs to" :function
+            (lambda (msg)
+              (let ((maildir
+                     (mu4e-message-field msg :maildir)))
+                (mu4e-header-colourise (replace-regexp-in-string "^gmail" (propertize "g" 'face 'bold-italic)
+                                                                 (format "%s"
+                                                                         (substring maildir 1
+                                                                                    (string-match-p "/" maildir 1)))))))))
+          (:human-date .
+           (:name "Human Date" :shortname "Date" :help "The date that the email was recived" :function
+            (lambda (msg)
+              (let ((maildir
+                     (mu4e-message-field msg :maildir)))
+                (mu4e-header-colourise)))))
+         
+          (:folder .
+           (:name "Folder" :shortname "Folder" :help "Lowest level folder" :function
+            (lambda (msg)
+              (let ((maildir
+                     (mu4e-message-field msg :maildir)))
+                (mu4e-header-colourise (replace-regexp-in-string "\\`.*/" "" maildir))))))
+          (:recipnum .
+           (:name "Number of recipients"
+            :shortname "#"
+            :help "Number of recipients for this message"
+            :function
+            (lambda (msg)
+              (propertize (format "%2d"
+                                  (+ (length (mu4e-message-field msg :to))
+                                     (length (mu4e-message-field msg :cc))))
+                          'face 'mu4e-footer-face)))))))
 
 (after! mu4e
   (defvar mu4e-min-header-frame-width 120
@@ -250,6 +292,13 @@
     (when (< (frame-width) mu4e-min-header-frame-width)
       (set-frame-width (selected-frame) mu4e-min-header-frame-width)))
   (add-hook 'mu4e-headers-mode-hook #'mu4e-widen-frame-maybe))
+
+(map! :map mu4e-headers-mode-map
+    :after mu4e
+    :v "*" #'mu4e-headers-mark-for-something
+    :v "!" #'mu4e-headers-mark-for-read
+    :v "?" #'mu4e-headers-mark-for-unread
+    :v "u" #'mu4e-headers-mark-for-unmark)
 
 (defadvice! mu4e~main-action-prettier-str (str &optional func-or-shortcut)
  "Highlight the first occurrence of [.] in STR.
@@ -317,12 +366,12 @@ clicked."
   (setq rmh-elfeed-org-files (list (concat org-directory "elfeed.org"))) ;; +org
   (add-hook! 'elfeed-search-mode-hook 'elfeed-update))
 
-(use-package! elfeed-goodies
-  :config
-  (elfeed-goodies/setup))
+;; (use-package! elfeed-goodies
+;;   :config
+;;   (elfeed-goodies/setup))
 
 (map!
- :n "zw" 'save-buffer ; = :w ZZ = :wq
+ :n "z C-w" 'save-buffer ; = :w ZZ = :wq handy
  :leader
   :desc "Enable Coloured Values""t c" #'rainbow-mode
   :desc "Toggle Tabs""t B" #'centaur-tabs-local-mode
