85d0d9f50d0751736a4d04b786d18734df5f6f5f
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2020-12-30 18:45:10 +0000

    moving to chemacs2

--- emacs/.config/chemacs/profiles.el
+++ emacs/.config/chemacs/profiles.el
@@ -3,10 +3,10 @@
 ;;; yeey
 ;;; Code:
 
-(("default"   . ((user-emacs-directory . "~/.config/emacs")))
+(("default"   . ((user-emacs-directory . "~/.config/emacs.doom")))
  ("mine"      . ((user-emacs-directory . "~/emacs/myemacs/")))
  ("spacemacs" . ((user-emacs-directory . "~/emacs/spacemacs")))
- ("nano"      . ((user-emacs-directory . "~/emacs/nano-emacs"))))    ;; my own config
+ ("nano"      . ((user-emacs-directory . "~/emacs/nano-emacs"))))   
 
 
 ;;; .emacs-profiles.el ends here
