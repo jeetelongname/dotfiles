602706d96a8fe64cc0899385c577a3ddb91a69ca
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2021-01-11 20:25:30 +0000

    adding comments to my profiles.el

--- emacs/.config/chemacs/profiles.el
+++ emacs/.config/chemacs/profiles.el
@@ -1,12 +1,13 @@
 ;;  package --- a bootloader configuarion for emacs
 ;;; Commentary:
 ;;; This is my profiles.el found in .config/chemacs
+;;; The default will load normally whole
 ;;; Code:
 
 (("default"   . ((user-emacs-directory . "~/.config/emacs.doom")))
- ("mine"      . ((user-emacs-directory . "~/emacs/myemacs/")))
- ("spacemacs" . ((user-emacs-directory . "~/emacs/spacemacs")))
- ("nano"      . ((user-emacs-directory . "~/emacs/nano-emacs"))))   
+ ("mine"      . ((user-emacs-directory . "~/emacs/myemacs/"))) ;; probably will become a test bed
+ ("spacemacs" . ((user-emacs-directory . "~/emacs/spacemacs")))) ;; need to remove spacemacs I am not giving it a fair go (doom is just that good)
+ ;; ("nano"      . ((user-emacs-directory . "~/emacs/nano-emacs")))) ;; nano does not have an init file
 
 
 ;;; .emacs-profiles.el ends here
