69cb5a9a84b877029b550920e758ddb7efc0c500
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2021-02-27 20:53:05 +0000

    moving tmux into home manager

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,8 +1,21 @@
 { config, lib, pkgs, ... }:
 
 {
-  # home.packages = with pkgs; [ alacritty ];
+  home.packages = with pkgs; [ alacritty ];
 
   xdg.configFile."alacritty/alacritty.yml".source =
     ../config/terminal/alacritty.yml;
+
+  # TODO: move more of the config into home manager
+  programs.tmux = {
+    enable = true;
+    # config
+    aggressiveResize = true;
+    baseIndex = 1;
+    clock24 = false;
+    historyLimit = 30000;
+    keyMode = "vi";
+
+    extraConfig = "source-file ~/.config/nixpkgs/config/terminal/tmux.conf";
+  };
 }
